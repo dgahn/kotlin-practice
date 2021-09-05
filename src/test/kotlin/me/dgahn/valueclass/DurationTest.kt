aab9b5abeb661d71d4e55aad79ad3890f58de785
dgahn <dgahn@si-analytics.ai>
2021-09-05 16:54:02 +0900

    feat: add first coroutine example

--- src/test/kotlin/me/dgahn/valueclass/DurationTest.kt
+++ src/test/kotlin/me/dgahn/valueclass/DurationTest.kt
@@ -2,10 +2,13 @@ package me.dgahn.valueclass
 
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
+import kotlinx.coroutines.delay
 
 class DurationTest: FunSpec({
 
     test("millisec와 sec로 Duration을 구할 수 있다.") {
+        println(1)
+        delay(1_000L)
         Duration.millis(10_000) shouldBe Duration.seconds(10)
     }
 
